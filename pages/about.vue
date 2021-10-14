60b9a725d37b95cc5fcf868cb18968ba34ea03fd
Cyebukayire <peacecyebukayire@gmail.com>
2021-10-14 20:32:34 +0200

    Added and-design-vue

---
+++ pages/about.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h2>Header</h2>
+        <h3>{{message}}</h3>
+        <h4>Working from {{location}}</h4>
+    </div>
+</template>
+
+<script lang="ts">
+import vue from 'vue'
+
+export default vue.extend({
+    data() {
+        return {
+            message: 'We are PayingTon',
+            location: 'Rwanda',            
+        }
+    },
+
+    computed: {
+        location (): string {
+            return `${this.location}`
+        }
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
